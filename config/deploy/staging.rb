408df0e403fb514511ae18c22ad1c9551cd51366
Raghu1320 <raghu@vt.edu>
2020-02-14 15:15:47 -0500

    Add production server details

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,9 +6,9 @@ set :stage, :staging
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@128.173.236.221}
-role :web, %w{deploy@128.173.236.221}
-role :db,  %w{deploy@128.173.236.221}
+role :app, %w{deploy@10.0.2.15}
+role :web, %w{deploy@10.0.2.15}
+role :db,  %w{deploy@10.0.2.15}
 
 
 # Extended Server Syntax
@@ -16,7 +16,7 @@ role :db,  %w{deploy@128.173.236.221}
 # This can be used to drop a more detailed server definition into the
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
-server '128.173.236.221', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+server '10.0.2.15', user: 'deploy', roles: %w{web app db}, my_property: :my_value
 
 
 # Custom SSH Options
